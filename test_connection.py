90252e883e4b0bd319d22b61204063b79751bcc6
FarzanehMasoumiyan <30278367+FarzanehMasoumiyan@users.noreply.github.com>
2026-08-20 13:36:14 -0400

    Update subscription ID in test_connection.py

--- test_connection.py
+++ test_connection.py
@@ -3,7 +3,7 @@ from azure.identity import DefaultAzureCredential
 
 ml_client = MLClient(
     credential=DefaultAzureCredential(),
-    subscription_id="ed3cde43-f0b0-44b7-89b1-8e27bc2a1fc8",
+    subscription_id="e",
     resource_group_name="rg-ml-journey",
     workspace_name="mlw-ml-journey",
 )
